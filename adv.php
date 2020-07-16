467bd57d10b45453b1d2633b74e3975d49ecf0a4
Antonazzo Nicola <nicola.antonazzo@corvallis.it>
2020-07-16 15:17:17 +0200

    updated json and renaming

--- adv.php
+++ adv.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     require_once __DIR__ . '/vendor/autoload.php';
-    use Erebox\TextAdventureEngine\Engine;
+    use Erebox\TextAdventureEngine\TextAdventureEngine;
 
     $data = (isset($_REQUEST['data'])? $_REQUEST['data'] : null);
     $response = [];
@@ -20,12 +20,12 @@
         $adv = unserialize($_SESSION['adv']);
         if ($adv->ended()) {
             $game_json = file_get_contents($quest);
-            $adv = new Engine($game_json);
+            $adv = new TextAdventureEngine($game_json);
             $data = null;
         }
     } else {
         $game_json = file_get_contents($quest);
-        $adv = new Engine($game_json);
+        $adv = new TextAdventureEngine($game_json);
     }
 
     if ($data) {
